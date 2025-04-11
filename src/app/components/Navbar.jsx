4b30d5484429ffcf440b20be8aba77fce053e043
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-04-11 19:25:12 +0530

    Initial commit from Create Next App

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+'use client';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header className="bg-gray-800 text-gray-50 flex justify-between gap-2 h-16 items-center px-5">
+      <h3 className="font-semibold text-2xl">AmazingCart</h3>
+      <nav className="flex items-center gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/shop">Shop</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
